64067d4a7de524d1a375f38a6f7ddf5462478911
Kees Meijer <keesie.meijer@gmail.com>
2018-10-18 20:04:44 +0200

    Remove PHP 5.3

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,14 +1,2 @@
 <?php
-
-// backward compatibility
-if ( version_compare( PHP_VERSION, '5.3', '=' ) ) {
-	if ( ! class_exists( 'PHPUnit\Framework\TestCase' ) && class_exists( 'PHPUnit_Framework_TestCase' ) ) {
-		class_alias( '\PHPUnit_Framework_TestCase', 'PHPUnit\Framework\TestCase' );
-	}
-} else {
-	if ( ! class_exists( '\PHPUnit\Framework\TestCase' ) && class_exists( '\PHPUnit_Framework_TestCase' ) ) {
-		class_alias( '\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase' );
-	}
-}
-
 require dirname( dirname( __FILE__ ) ) . '/insert-content.php';
