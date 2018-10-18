f68d0d681053b4d5b26d3b132560c9664fc8a4e1
Kees Meijer <keesie.meijer@gmail.com>
2018-10-18 18:33:58 +0200

    Use unslashed namespace

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,10 +1,12 @@
 <?php
 
 // backward compatibility
-if ( ! class_exists( '\PHPUnit\Framework\TestCase' ) && class_exists( '\PHPUnit_Framework_TestCase' ) ) {
-	if ( version_compare( PHP_VERSION, '5.3', '<=' ) ) {
+if ( version_compare( PHP_VERSION, '5.3', '=' ) ) {
+	if ( ! class_exists( 'PHPUnit\Framework\TestCase' ) && class_exists( '\PHPUnit_Framework_TestCase' ) ) {
 		class_alias( '\PHPUnit_Framework_TestCase', 'PHPUnit\Framework\TestCase' );
-	} else {
+	}
+} else {
+	if ( ! class_exists( '\PHPUnit\Framework\TestCase' ) && class_exists( '\PHPUnit_Framework_TestCase' ) ) {
 		class_alias( '\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase' );
 	}
 }
