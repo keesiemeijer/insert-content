160ba041fc6d180ffe5b5d44648fc8820ac39102
Kees Meijer <keesie.meijer@gmail.com>
2018-10-18 17:49:20 +0200

    Add backward compatibility for travis

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,2 +1,12 @@
 <?php
+
+// backward compatibility
+if ( ! class_exists( '\PHPUnit\Framework\TestCase' ) && class_exists( '\PHPUnit_Framework_TestCase' ) ) {
+	if ( version_compare( PHP_VERSION, '5.3', '<=' ) ) {
+		class_alias( '\PHPUnit_Framework_TestCase', 'PHPUnit\Framework\TestCase' );
+	} else {
+		class_alias( '\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase' );
+	}
+}
+
 require dirname( dirname( __FILE__ ) ) . '/insert-content.php';
